e6ee8e6da1407c623410c9bbbb5fcdca49af5b60
OtterBell <r08625028@ntu.edu.tw>
2025-04-11 10:42:59 +0800

    Initial Commit

---
+++ HW4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW4
+//
+//  Created by Mac11 on 2025/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by Mac11 on 2025/4/11.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
